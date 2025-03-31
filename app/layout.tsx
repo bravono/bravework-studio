05018582767ec5e9b738d2016eecea9c79cda182
Ahbideen Yusuf <ahbideeny@gmail.com>
2025-03-31 03:00:39 +0100

    Add a website icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,6 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <link rel="icon" type="image/png" sizes="32x32" href="/Bravework_Studio-Logo_black-Transparent-bg.png"></link>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" />
       </head>
       <body className={inter.className}>
