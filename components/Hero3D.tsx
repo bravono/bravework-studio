03908a1b288b69031df17a290874198ca3e4a1f1
Ahbideen Yusuf <ahbideeny@gmail.com>
2025-04-27 21:03:04 +0100

    Play the animation in the glb file and style hero component for mobile

--- components/Hero3D.tsx
+++ components/Hero3D.tsx
@@ -1,8 +1,8 @@
 "use client";
 
-import React, { useRef } from "react";
+import React, { useRef, useEffect } from "react";
 import { useFrame } from "@react-three/fiber";
-import { Mesh } from "three";
+import { Mesh, AnimationMixer } from "three";
 import { useGLTF } from "@react-three/drei";
 
 export default function Hero3D() {
@@ -15,12 +15,33 @@ export default function Hero3D() {
     }
   });
 
+  const isMobile = typeof window !== "undefined" && window.innerWidth <= 768;
+
+  const { scene, animations } = useGLTF("assets/Isometric-Office.glb");
+  const mixerRef = useRef<THREE.AnimationMixer | null>(null);
+
+  useEffect(() => {
+    if (animations && animations.length > 0) {
+      mixerRef.current = new AnimationMixer(scene);
+      const action = mixerRef.current.clipAction(animations[0]);
+      action.play();
+    }
+
+    return () => {
+      mixerRef.current?.stopAllAction();
+    };
+  }, [scene, animations]);
+
+  useFrame((state, delta) => {
+    mixerRef.current?.update(delta);
+  });
+
   return (
     <primitive
-      object={useGLTF("assets/Isometric-Office.glb").scene}
-      scale={2.5}
-      position={[1, -1.3, 5]}
-      rotation={[0, -0.55, 0]}
+      object={scene}
+      scale={isMobile ? 8 : 2.5}
+      position={isMobile ? [0, -8, 0] : [1, -1.3, 5]}
+      rotation={isMobile ? [0, -0.7, 0] : [0, -0.55, 0]}
     />
   );
 }
