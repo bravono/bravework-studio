e68f2ca8c25590d9d4d12e2054a67300359524a7
Ahbideen Yusuf <ahbideeny@gmail.com>
2025-09-13 23:25:22 +0100

    Style: Use my color palate with tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,32 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
     "./app/**/*.{js,ts,jsx,tsx,mdx}", // Adjust this based on your project structure
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     // Add any other directories where you use Tailwind classes
   ],
   theme: {
-    extend: {},
+    extend: {
+       colors: {
+        primary: {
+          DEFAULT: 'var(--primary-color)',
+          light: 'var(--primary-light)',
+          dark: 'var(--primary-dark)',
+
+        },
+        secondary: {
+          DEFAULT: 'var(--secondary-color)',
+          light: 'var(--secondary-light)',
+          dark: 'var(--secondary-dark)',
+        },
+        tertiary: {
+          DEFAULT: 'var(--tertiary-color)',
+          light: 'var(--tertiary-light)',
+          dark: 'var(--tertiary-dark)',
+        },
+      },
+    },
   },
   plugins: [],
 };
