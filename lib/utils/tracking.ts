a928ad3c78ee968c6b83c707881e70bdca2219f6
Ahbideen Yusuf <ahbideeny@gmail.com>
2025-10-07 14:52:57 +0100

    Remove white spaces in tracking ID

--- lib/utils/tracking.ts
+++ lib/utils/tracking.ts
@@ -1,5 +1,5 @@
 export const createTrackingId = (serviceType: string): string => {
-  const prefix = serviceType.slice(0, 3).toUpperCase();
+  const prefix = serviceType.replace(/\s+/g, '').slice(0, 3).toUpperCase();
   const suffix = Date.now().toString().slice(-6);
   return `${prefix}-${suffix}`;
 };
