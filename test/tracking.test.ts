f119204e520cb2ca49d402a2172b6348ab49922a
Ahbideen Yusuf <ahbideeny@gmail.com>
2025-12-23 12:51:41 +0100

    Test: Ensure the first 3 character is alphanumeric

--- test/tracking.test.ts
+++ test/tracking.test.ts
@@ -1,5 +1,5 @@
 import { createTrackingId } from "@/lib/utils/tracking";
 
-test("should create a tracking ID with first 3 characters of service type as prefix and first 6 digits of current timestamp as suffix", () => {
-  expect(createTrackingId("Courier")).toMatch(/^[A-Za-z]{3}-\d{6}$/g);
+test("createTrackingId - should create a tracking ID with 3 alphanumeric characters, a dash followed by 6 digits", () => {
+  expect(createTrackingId("3D Animation")).toMatch(/^[A-Za-z0-9]{3}-\d{6}$/g);
 });
