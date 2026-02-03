d870ea2d167e53182a0c2a4fcc81358a14f66dbe
Ahbideen Yusuf <ahbideeny@gmail.com>
2026-02-03 12:31:50 +0100

    Cleanup: Remove redundant migration code

--- prisma/migrations/20260127172836_update_user_email/migration.sql
+++ prisma/migrations/20260127172836_update_user_email/migration.sql
@@ -10,14 +10,8 @@
 DROP INDEX "orders_user_category_idx";
 
 -- AlterTable
-ALTER TABLE "budget_ranges" DROP CONSTRAINT "budget_ranges_pkey",
-DROP COLUMN "range_label";
-
--- AlterTable
 ALTER TABLE "users" ADD COLUMN     "pending_email" VARCHAR(255);
 
 -- CreateIndex
 CREATE UNIQUE INDEX "users_pending_email_key" ON "users"("pending_email");
 
--- AddForeignKey
-ALTER TABLE "product_budget_ranges" ADD CONSTRAINT "constraint_1" FOREIGN KEY ("project_category_id") REFERENCES "product_categories"("category_id") ON DELETE NO ACTION ON UPDATE NO ACTION;
